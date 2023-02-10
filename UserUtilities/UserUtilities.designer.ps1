3a4fb43f32dff036e92c109fe25192bad9f8b9b1
0xAnthonyW <waltersanthony17@gmail.com>
2023-02-10 11:44:48 -0600

    not done WIP.

--- UserUtilities/UserUtilities.designer.ps1
+++ UserUtilities/UserUtilities.designer.ps1
@@ -1,22 +1,22 @@
 ﻿[void][System.Reflection.Assembly]::Load('System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a')
 [void][System.Reflection.Assembly]::Load('System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
 $UserUtilities = New-Object -TypeName System.Windows.Forms.Form
-[System.Windows.Forms.ListBox]$listBox1 = $null
 [System.Windows.Forms.TabPage]$Main = $null
 [System.Windows.Forms.Button]$NetwrkBtn = $null
 [System.Windows.Forms.Button]$RTclockBtn = $null
 [System.Windows.Forms.Button]$PWexpireBtn = $null
 [System.Windows.Forms.Button]$PWresetBtn = $null
+[System.Windows.Forms.ListBox]$output = $null
 [System.Windows.Forms.TabControl]$tabControl1 = $null
 function InitializeComponent
 {
 $tabControl1 = (New-Object -TypeName System.Windows.Forms.TabControl)
 $Main = (New-Object -TypeName System.Windows.Forms.TabPage)
-$listBox1 = (New-Object -TypeName System.Windows.Forms.ListBox)
-$PWresetBtn = (New-Object -TypeName System.Windows.Forms.Button)
-$PWexpireBtn = (New-Object -TypeName System.Windows.Forms.Button)
-$RTclockBtn = (New-Object -TypeName System.Windows.Forms.Button)
 $NetwrkBtn = (New-Object -TypeName System.Windows.Forms.Button)
+$RTclockBtn = (New-Object -TypeName System.Windows.Forms.Button)
+$PWexpireBtn = (New-Object -TypeName System.Windows.Forms.Button)
+$PWresetBtn = (New-Object -TypeName System.Windows.Forms.Button)
+$output = (New-Object -TypeName System.Windows.Forms.ListBox)
 $tabControl1.SuspendLayout()
 $Main.SuspendLayout()
 $UserUtilities.SuspendLayout()
@@ -36,7 +36,7 @@ $Main.Controls.Add($NetwrkBtn)
 $Main.Controls.Add($RTclockBtn)
 $Main.Controls.Add($PWexpireBtn)
 $Main.Controls.Add($PWresetBtn)
-$Main.Controls.Add($listBox1)
+$Main.Controls.Add($output)
 $Main.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]4,[System.Int32]22))
 $Main.Name = [System.String]'Main'
 $Main.Padding = (New-Object -TypeName System.Windows.Forms.Padding -ArgumentList @([System.Int32]3))
@@ -44,41 +44,6 @@ $Main.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.I
 $Main.TabIndex = [System.Int32]0
 $Main.Text = [System.String]'Main'
 $Main.UseVisualStyleBackColor = $true
-#
-#listBox1
-#
-$listBox1.FormattingEnabled = $true
-$listBox1.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]116,[System.Int32]6))
-$listBox1.Name = [System.String]'listBox1'
-$listBox1.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]343,[System.Int32]212))
-$listBox1.TabIndex = [System.Int32]0
-#
-#PWresetBtn
-#
-$PWresetBtn.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]6,[System.Int32]6))
-$PWresetBtn.Name = [System.String]'PWresetBtn'
-$PWresetBtn.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]104,[System.Int32]25))
-$PWresetBtn.TabIndex = [System.Int32]1
-$PWresetBtn.Text = [System.String]'Password Reset'
-$PWresetBtn.UseVisualStyleBackColor = $true
-#
-#PWexpireBtn
-#
-$PWexpireBtn.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]6,[System.Int32]37))
-$PWexpireBtn.Name = [System.String]'PWexpireBtn'
-$PWexpireBtn.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]104,[System.Int32]25))
-$PWexpireBtn.TabIndex = [System.Int32]2
-$PWexpireBtn.Text = [System.String]'Password Expire'
-$PWexpireBtn.UseVisualStyleBackColor = $true
-#
-#RTclockBtn
-#
-$RTclockBtn.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]6,[System.Int32]68))
-$RTclockBtn.Name = [System.String]'RTclockBtn'
-$RTclockBtn.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]104,[System.Int32]25))
-$RTclockBtn.TabIndex = [System.Int32]3
-$RTclockBtn.Text = [System.String]'Real Time Clock'
-$RTclockBtn.UseVisualStyleBackColor = $true
 #
 #NetwrkBtn
 #
@@ -88,6 +53,46 @@ $NetwrkBtn.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([Sys
 $NetwrkBtn.TabIndex = [System.Int32]4
 $NetwrkBtn.Text = [System.String]'Wi-Fi Reset'
 $NetwrkBtn.UseVisualStyleBackColor = $true
+$NetwrkBtn.add_Click($NetwrkBtn_Click)
+#
+#RTclockBtn
+#
+$RTclockBtn.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]6,[System.Int32]68))
+$RTclockBtn.Name = [System.String]'RTclockBtn'
+$RTclockBtn.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]104,[System.Int32]25))
+$RTclockBtn.TabIndex = [System.Int32]3
+$RTclockBtn.Text = [System.String]'Real Time Clock'
+$RTclockBtn.UseVisualStyleBackColor = $true
+$RTclockBtn.add_Click($RTclockBtn_Click)
+#
+#PWexpireBtn
+#
+$PWexpireBtn.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]6,[System.Int32]37))
+$PWexpireBtn.Name = [System.String]'PWexpireBtn'
+$PWexpireBtn.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]104,[System.Int32]25))
+$PWexpireBtn.TabIndex = [System.Int32]2
+$PWexpireBtn.Text = [System.String]'Password Expire'
+$PWexpireBtn.UseVisualStyleBackColor = $true
+$PWexpireBtn.add_Click($PWexpireBtn_Click)
+#
+#PWresetBtn
+#
+$PWresetBtn.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]6,[System.Int32]6))
+$PWresetBtn.Name = [System.String]'PWresetBtn'
+$PWresetBtn.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]104,[System.Int32]25))
+$PWresetBtn.TabIndex = [System.Int32]1
+$PWresetBtn.Text = [System.String]'Password Reset'
+$PWresetBtn.UseVisualStyleBackColor = $true
+$PWresetBtn.add_Click($PWresetBtn_Click)
+#
+#output
+#
+$output.FormattingEnabled = $true
+$output.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]116,[System.Int32]6))
+$output.Name = [System.String]'output'
+$output.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]343,[System.Int32]212))
+$output.TabIndex = [System.Int32]0
+$output.add_SelectedIndexChanged($output_SelectedIndexChanged)
 #
 #UserUtilities
 #
@@ -102,12 +107,12 @@ $UserUtilities.add_Load($UserUtilities_Load)
 $tabControl1.ResumeLayout($false)
 $Main.ResumeLayout($false)
 $UserUtilities.ResumeLayout($false)
-Add-Member -InputObject $UserUtilities -Name listBox1 -Value $listBox1 -MemberType NoteProperty
 Add-Member -InputObject $UserUtilities -Name Main -Value $Main -MemberType NoteProperty
 Add-Member -InputObject $UserUtilities -Name NetwrkBtn -Value $NetwrkBtn -MemberType NoteProperty
 Add-Member -InputObject $UserUtilities -Name RTclockBtn -Value $RTclockBtn -MemberType NoteProperty
 Add-Member -InputObject $UserUtilities -Name PWexpireBtn -Value $PWexpireBtn -MemberType NoteProperty
 Add-Member -InputObject $UserUtilities -Name PWresetBtn -Value $PWresetBtn -MemberType NoteProperty
+Add-Member -InputObject $UserUtilities -Name output -Value $output -MemberType NoteProperty
 Add-Member -InputObject $UserUtilities -Name tabControl1 -Value $tabControl1 -MemberType NoteProperty
 }
 . InitializeComponent
--- UserUtilities/UserUtilities.ps1
+++ UserUtilities/UserUtilities.ps1
@@ -1,3 +1,70 @@
+$NetwrkBtn_Click = {
+    Restart-NetAdapter -Name "Wi*"
+    $output.Items.Add("Restart-NetAdapter")
+    $output.Items.Add("Network adapter restarted")
+    # Clears Dns Cache
+    Clear-DnsClientCache
+    Start-Sleep -Seconds 5
+    ipconfig /flushdns
+    $output.Items.Add("Clear-DnsClientCache")
+    $output.Items.Add("ipconfig /flushdns")
+    $output.Items.Add("Flushed DNS")
+    # Release and Renew the ip configuration
+    Start-Sleep -Seconds 5
+    ipconfig /Release
+    $output.Items.Add("ipconfig /Release")
+    Start-Sleep -Seconds 5
+    ipconfig /Renew
+    $output.Items.Add("ipconfig /Renew")
+    $output.Items.Add("IP configuration released & renewed!")
+    netsh winsock reset
+    $output.Items.Add("netsh winsock reset")
+    Start-Sleep -Seconds 5
+    netcfg -d
+    $output.Items.Add("netcfg -d")
+    $output.Items.Add("Network adapter has been Reset")
+    Start-Sleep -Seconds 4
+    $output.Items.Add("Done. Restarting in 4 Seconds.")
+    Restart-Computer -force
+}
+
+$RTclockBtn_Click = {
+    Set-TimeZone -Name 'Central Standard Time' -PassThru
+    $output.Items.Add("Set-TimeZone -Name 'Central Standard Time' -PassThru")
+    $output.Items.Add("Done.")
+    [System.Windows.Forms.MessageBox]::Show("Timezone has been set to CST")
+}
+
+$PWexpireBtn_Click = {
+    $user = (Get-LocalUser | Where-Object { $_.Name -like "STU*" }).Name
+    Set-LocalUser -Name $user -PasswordNeverExpires $True
+    $output.Items.Add("Set-LocalUser -Name $user -PasswordNeverExpires $True")
+    $output.Items.Add("Done.")
+    [System.Windows.Forms.MessageBox]::Show("Password for $user has been set to not expire")
+}
+
+$output_SelectedIndexChanged = {
+
+}
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
+}
+$PWresetBtn_Click = {
+     $user = (Get-LocalUser | Where-Object { $_.Name -like "STU*" }).Name
+     $output.Items.Add("Get-LocalUser $user")
+	 #Param([string]$user)
+     # Sets the Password Never Expires to false to ensure logonpasswordchg can be set
+     Set-LocalUser -Name $user -PasswordNeverExpires $False
+     $output.Items.Add("Set-LocalUser -name $user -PasswordNeverExpires $False")
+     # Resets the password of the user to 'Student'
+     net user $user "Student" /logonpasswordchg:yes
+     $output.Items.Add("$user /logonpasswordchg:yes")
+     $output.Items.Add("Done.")
+     [System.Windows.Forms.MessageBox]::Show("Password for user $user has been reset to 'Student' and set to user must change password at next login")
+}
+
 $UserUtilities_Load = {
 
 }
